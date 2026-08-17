9683ae5cb4f43bc71e76652b5b85b9f2347605fb
Yuvasree-01 <121015977+Yuvasree-01@users.noreply.github.com>
2026-08-17 23:59:56 +0530

    Time: 2 ms (64.92%), Space: 21 MB (93.52%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,9 +1,17 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         # Counter creates a frequency map of the list automatically
-        counts = Counter(nums)
+        # counts = Counter(nums)
         
-        # Look for the number with a count/frequency of 1
-        for num, count in counts.items():
-            if count == 1:
-                return num
+        # # Look for the number with a count/frequency of 1
+        # for num, count in counts.items():
+        #     if count == 1:
+        #         return num
+
+        result = 0
+        
+        # XOR every number together
+        for num in nums:
+            result ^= num
+            
+        return result
