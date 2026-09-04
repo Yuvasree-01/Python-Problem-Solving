981601f93600c2d7b5b8f5ee58f10e8fa91e0f7a
Yuvasree-01 <121015977+Yuvasree-01@users.noreply.github.com>
2026-09-04 20:00:25 +0530

    Time: 19 ms (73.22%), Space: 25.5 MB (69.88%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,20 +1,36 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prod=[]
-        product=1
-        zero=0
-        for num in nums:
-            if num==0:
-                zero+=1
-            else:
-                product*=num
-        for i in nums:
-            if zero==0:
-                prod.append(int(product/i))
-            elif i==0 and zero==1:
-                prod.append(int(product))
-            elif i!=0 and zero>=1:
-                prod.append(0)
-            else:
-                prod.append(0)
-        return prod
+        #Non division method:
+        left=[]
+        left_product=1
+        right_product=1
+        for i in range(len(nums)):
+            left.append(left_product)
+            left_product=left_product*nums[i]
+        for j in range(len(nums)-1,-1,-1):
+            left[j]=left[j]* right_product
+            right_product=nums[j]*right_product
+        return left
+            
+
+
+        #Division method:
+
+        # prod=[]
+        # product=1
+        # zero=0
+        # for num in nums:
+        #     if num==0:
+        #         zero+=1
+        #     else:
+        #         product*=num
+        # for i in nums:
+        #     if zero==0:
+        #         prod.append(int(product/i))
+        #     elif i==0 and zero==1:
+        #         prod.append(int(product))
+        #     elif i!=0 and zero>=1:
+        #         prod.append(0)
+        #     else:
+        #         prod.append(0)
+        # return prod
