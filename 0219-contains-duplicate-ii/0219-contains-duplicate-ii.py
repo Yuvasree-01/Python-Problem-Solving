bc4969a8241e1aa677e28a177c50a3e7ad02c6ab
Yuvasree-01 <121015977+Yuvasree-01@users.noreply.github.com>
2026-09-18 23:21:21 +0530

    Time: 39 ms (90.04%), Space: 39.1 MB (67.03%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,25 +1,10 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: list[int], k: int) -> bool:
-        # right=len(nums)-1
-        # for left in range(0,right):
-        #     if nums[left]==nums[right]:
-        #         if abs(left-right)<=k:
-        #             return True
-                
-        # return False
+        seen = {} 
         
-        window = set()
-        
-        for i in range(len(nums)):
-            # If the number is already in our window, we found a nearby duplicate
-            if nums[i] in window:
-                return True
+        for i, num in enumerate(nums):
+            if num in seen and i - seen[num] <= k:
+                return True 
+            seen[num] = i
             
-            # Add the current number to the window
-            window.add(nums[i])
-            
-            # If the window size exceeds k, remove the oldest element
-            if len(window) > k:
-                window.remove(nums[i - k])
-                
         return False
